464c3594a37506ccdf7fc43933f7902fff919208
makhnivchyk <makhnivchyk@gmail.com>
2021-07-06 23:53:11 +0300

    fix bags [1-7], 10, 12, 13

--- src/js/burger.js
+++ src/js/burger.js
@@ -2,7 +2,7 @@
 
 $(document).ready(function() {
     $('.header__burger').click(function() {
-        $('.header__burger, .nav, .intro, header').toggleClass('active')
+        $('.header__burger, .nav, .intro, header, .header__logo, .header__icons').toggleClass('active')
         $('body').toggleClass('lock')         
     })
     $('li').children('ul.nav__sub-list').parent().children('a').addClass("link__arrow")
